0c716f662aa95cc1afe9db5445c34532a533e822
Anatoly Shulgin <anatoly.sh@abra-it.com>
2026-05-05 15:46:12 +0300

    remove all base44 related data from the project and make some small changes to run build successfuly

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,6 @@
 import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
 import tailwind from '@astrojs/tailwind';
-import base44 from "@base44/vite-plugin";
 
 // https://astro.build/config
 export default defineConfig({
@@ -9,17 +8,6 @@ export default defineConfig({
     applyBaseStyles: false,
   })],
   vite: {
-    plugins: [
-      base44({
-        // Support for legacy code that imports the base44 SDK with @/integrations, @/entities, etc.
-        // can be removed if the code has been updated to use the new SDK imports from @base44/sdk
-        legacySDKImports: process.env.BASE44_LEGACY_SDK_IMPORTS === 'true',
-        hmrNotifier: true,
-        navigationNotifier: true,
-        analyticsTracker: true,
-        visualEditAgent: true
-      })
-    ],
     resolve: {
       alias: {
         '@': '/src'
--- src/api/base44Client.js
+++
@@ -1,7 +0,0 @@
-import { createClient } from '@base44/sdk';
-import { appParams } from '@/lib/app-params';
-
-export const base44 = createClient({
-  appId: appParams.appId,
-  token: appParams.token,
-});
